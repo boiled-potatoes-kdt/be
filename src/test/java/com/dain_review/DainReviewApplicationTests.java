968f93818134e115458a0e193730442e18c8173b
Domae-back-end <ty_ty123@naver.com>
2024-07-26 11:16:20 +0900

    init

---
+++ src/test/java/com/dain_review/DainReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dain_review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DainReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
