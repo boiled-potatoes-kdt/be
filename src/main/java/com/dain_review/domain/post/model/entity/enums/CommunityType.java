c48a3155594d4491c1d34dcf64bdc6053049b5b9
leeshinbi <tlsql6831@naver.com>
2024-08-19 13:26:43 +0900

    fix: 충돌 해결

--- src/main/java/com/dain_review/domain/post/model/entity/enums/CommunityType.java
+++ src/main/java/com/dain_review/domain/post/model/entity/enums/CommunityType.java
@@ -7,11 +7,7 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum CommunityType {
-<<<<<<< HEAD
-    QnA("질문하기"),
-=======
     QUESTION("질문하기"),
->>>>>>> 37241ba30cfc18659fdaacb018cdba4b77cb38dd
     KNOWHOW("노하우"),
     COMPANION("동행"),
     ETC("기타");
