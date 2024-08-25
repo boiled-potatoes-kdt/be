12e3f12974c8b3d844470da2c6fdbbebb6c0ec15
song <ssogg5309@naver.com>
2024-08-25 23:50:20 +0900

    feat: 마이페이지 관련 application 도메인

---
+++ src/main/java/com/dain_review/domain/application/exception/ApplicationErrorCode.java
@@ -0,0 +1,17 @@
+package com.dain_review.domain.application.exception;
+
+
+import com.dain_review.global.exception.ErrorCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum ApplicationErrorCode implements ErrorCode {
+    FAIL_CANCEL(HttpStatus.BAD_REQUEST, "리뷰마감 단계에서는 취소할 수 없습니다."),
+    NOT_FOUND_BY_ID_USERID(HttpStatus.BAD_REQUEST, "해당 (id, userId) 의 신청이 없습니다.");
+
+    private final HttpStatus status;
+    private final String msg;
+}
--- src/main/java/com/dain_review/domain/application/model/entity/Application.java
+++ src/main/java/com/dain_review/domain/application/model/entity/Application.java
@@ -3,6 +3,8 @@ package com.dain_review.domain.application.model.entity;
 
 import com.dain_review.domain.application.model.entity.enums.State;
 import com.dain_review.domain.campaign.model.entity.Campaign;
+import com.dain_review.domain.campaign.model.entity.enums.CampaignState;
+import com.dain_review.domain.campaign.model.response.CampaignResponse;
 import com.dain_review.domain.user.model.entity.User;
 import com.dain_review.global.model.entity.BaseEntity;
 import jakarta.persistence.Entity;
@@ -10,6 +12,8 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,4 +38,45 @@ public class Application extends BaseEntity {
 
     @Enumerated(EnumType.STRING)
     private State state;
+
+    private Boolean isDeleted;
+
+    public void delete() {
+        this.isDeleted = true;
+    }
+
+    public CampaignResponse toCampaignResponse() {
+
+        // 지원 마감까지 남은 일수
+        LocalDateTime now = LocalDateTime.now();
+        Duration duration = Duration.between(now, this.campaign.getApplicationEndDate());
+        Long applicationDeadline = Math.max(duration.toDays(), 0);
+
+        // 취소가능한지 여부 - 모집중, 모집완료, 체험&리뷰 단계에서만 가능
+        Boolean isCancel = !CampaignState.REVIEW_CLOSED.equals(this.campaign.getCampaignState());
+
+        // 찜한 캠페인인지
+        // Todo - 이부분을 어떻게 구현해야할지
+        //        this.campaign.getLikeList().
+
+        // CampaignResponse 로 변환
+        return new CampaignResponse(
+                this.campaign.getId(),
+                this.campaign.getUser().getNickname(),
+                this.campaign.getReward(),
+                this.campaign.getRegion1(),
+                this.campaign.getRegion2(),
+                this.campaign.getType(),
+                this.campaign.getPlatform(),
+                this.campaign.getUser().getProfileImage(),
+                this.campaign.getCampaignState(),
+                this.campaign.getCapacity(),
+                this.campaign.getApplicant(),
+                this.campaign.getExperienceStartDate(),
+                this.campaign.getExperienceEndDate(),
+                applicationDeadline,
+                isCancel,
+                this.campaign.getLabel(),
+                false);
+    }
 }
