682dd7d6f5c697a4e2bac22e77d7caa907d13a33
leeshinbi <tlsql6831@naver.com>
2024-08-26 19:01:44 +0900

    refactor: DeleteMapping -> PatchMapping으로 변경

--- src/main/java/com/dain_review/domain/campaign/controller/CampaignController.java
+++ src/main/java/com/dain_review/domain/campaign/controller/CampaignController.java
@@ -10,8 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +46,7 @@ public class CampaignController {
     }
 
     @PreAuthorize("hasAnyRole('ROLE_ENTERPRISER')")
-    @DeleteMapping("/{campaignId}") // 체험단 삭제(취소)
+    @PatchMapping("/{campaignId}") // 체험단 삭제(취소)
     public ResponseEntity<?> deleteCampaign(
             @AuthenticationPrincipal CustomUserDetails customUserDetails,
             @PathVariable Long campaignId) {
