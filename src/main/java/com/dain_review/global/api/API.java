82838a5f83065ea170890cde85d603949ed7b15e
EomHyeYeong <hy30051@naver.com>
2024-08-30 18:28:36 +0900

    feat: 접근 권한에 대한 에러처리

--- src/main/java/com/dain_review/global/api/API.java
+++ src/main/java/com/dain_review/global/api/API.java
@@ -6,6 +6,8 @@ import com.dain_review.global.exception.GlobalResponse;
 import lombok.Data;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @Data
@@ -26,5 +28,10 @@ public class API {
     public static ResponseEntity ERROR(MethodArgumentNotValidException ex) {
         return ResponseEntity.status(400)
                 .body(new GlobalResponse("잘못된 인자값 입니다.", HttpStatus.valueOf(400)));
+    }
+
+    public static ResponseEntity ERROR(AccessDeniedException ex) {
+        return ResponseEntity.status(403)
+                .body(new GlobalResponse("해당 기능에 대한 권한이 없습니다.", HttpStatus.valueOf(403)));
     }
 }
