3f02bb77ea1ce1e9a1e5114a3f22aa5bf24806df
EomHyeYeong <hy30051@naver.com>
2024-09-05 17:23:58 +0900

    feat: 리뷰 예외처리

---
+++ src/main/java/com/dain_review/domain/review/exception/ReviewException.java
@@ -0,0 +1,20 @@
+package com.dain_review.domain.review.exception;
+
+import com.dain_review.global.exception.ErrorCode;
+import com.dain_review.global.exception.GlobalException;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "리뷰 에러")
+public class ReviewException extends GlobalException {
+    private final ErrorCode errorCode;
+
+    public ReviewException(ErrorCode errorCode) {
+        super(errorCode);
+        this.errorCode = errorCode;
+    }
+
+    @Override
+    public void exceptionHandling() {
+        log.error(errorCode.getMsg() + " : " + errorCode.getStatus().value());
+    }
+}
