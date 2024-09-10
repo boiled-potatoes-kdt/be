418b2e4bb2cee08581c6f2c15d5e1e085b57644e
EomHyeYeong <hy30051@naver.com>
2024-09-11 02:24:24 +0900

    feat: 홈 화면 조회 기능 구현

--- src/main/java/com/dain_review/domain/campaign/controller/CampaignController.java
+++ src/main/java/com/dain_review/domain/campaign/controller/CampaignController.java
@@ -5,6 +5,7 @@ import com.dain_review.domain.application.model.response.ApplicantResponse;
 import com.dain_review.domain.campaign.model.request.CampaignFilterRequest;
 import com.dain_review.domain.campaign.model.request.CampaignRequest;
 import com.dain_review.domain.campaign.model.request.CampaignSearchRequest;
+import com.dain_review.domain.campaign.model.response.CampaignHomeResponse;
 import com.dain_review.domain.campaign.model.response.CampaignResponse;
 import com.dain_review.domain.campaign.model.response.CampaignSummaryResponse;
 import com.dain_review.domain.campaign.service.CampaignService;
@@ -123,5 +124,12 @@ public class CampaignController {
         List<ReviewerResponse> reviewerResponseList =
                 campaignService.getReviews(campaignId, customUserDetails.getUserId());
         return API.OK(reviewerResponseList);
+    }
+
+    // 홈 화면 조회
+    @GetMapping("/home")
+    public ResponseEntity<CampaignHomeResponse> getCampaignForEachCategory() {
+        CampaignHomeResponse campaignHomeResponse = campaignService.getCampaignForHomeScreen();
+        return API.OK(campaignHomeResponse);
     }
 }
